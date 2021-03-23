c69949e2a3d229bd1772f027d9a13d2a1536a1e8
COSSIGOMEZ <COSSIGOMEZ@192.168.0.33>
2021-03-22 22:56:16 -0300

    CRUD empleando Spring

---
+++ src/main/java/com/vzla/jc/web/ControladorInicio.java
@@ -0,0 +1,28 @@
+package com.vzla.jc.web;
+
+import com.vzla.jc.servicio.PersonaService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@Slf4j
+public class ControladorInicio {
+
+    @Autowired
+    private PersonaService personaService;
+    
+    @GetMapping("/")
+    
+    public String inicio(Model model) {
+        
+        var personas = personaService.listarPersonas();
+        log.info("Ejecutando el controlador Spring MVC");
+
+        model.addAttribute("personas", personas);
+        return "index";
+    }
+
+}
